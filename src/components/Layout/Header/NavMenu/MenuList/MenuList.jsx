654b3b2c7b06d7802b38044d1ce9894e57082f99
anthonyc-a <anthonyc.animba@gmail.com>
2022-02-09 14:18:49 +0000

    -

--- src/components/Layout/Header/NavMenu/MenuList/MenuList.jsx
+++ src/components/Layout/Header/NavMenu/MenuList/MenuList.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 const navLinks = {
   show: {
     transition: {
-      staggerChildren: 0.1,
+      staggerChildren: 0.2,
       delayChildren: 0.1,
     },
   },

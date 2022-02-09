db67975485d57705e011965b7e2aa09d62c62cee
anthonyc-a <anthonyc.animba@gmail.com>
2022-02-09 23:57:40 +0000

    home changes

--- src/components/Sections/Title/Description.jsx
+++ src/components/Sections/Title/Description.jsx
@@ -1,7 +1,30 @@
 import React from "react"
+import { motion } from "framer-motion"
+
+const appear = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      ease: [0.51, 0.92, 0.24, 1],
+      duration: 0.4,
+    },
+  },
+  show: {
+    opacity: 0.5,
+    transition: {
+      ease: [0.51, 0.92, 0.24, 1],
+      duration: 0.4,
+      delay: 0.4,
+    },
+  },
+}
 
 const Description = ({ description }) => {
-  return <span>{description}</span>
+  return (
+    <motion.span variants={appear} initial="hidden" animate="show">
+      {description}
+    </motion.span>
+  )
 }
 
 export default Description
--- src/components/Sections/Title/Title.jsx
+++ src/components/Sections/Title/Title.jsx
@@ -1,20 +1,19 @@
 import React from "react"
 import { motion } from "framer-motion"
 
-
-const img = {
+const rise = {
   hidden: {
-    width: "0%",
+    y: "100%",
     transition: {
       ease: [0.51, 0.92, 0.24, 1],
       duration: 0.4,
     },
   },
   show: {
-    width: "100%",
+    y: "0%",
     transition: {
       ease: [0.51, 0.92, 0.24, 1],
-      duration: 4,
+      duration: 0.4,
     },
   },
 }
@@ -22,7 +21,9 @@ const img = {
 const Title = ({ title }) => {
   return (
     <div className="title">
-      <h2>{title}</h2>
+      <motion.h2 variants={rise} initial="hidden" animate="show">
+        {title}
+      </motion.h2>
     </div>
   )
 }

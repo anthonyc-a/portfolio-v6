a0e51186b886360c3e7922f7b4148f771728fe5e
anthonyc-a <anthonyc.animba@gmail.com>
2022-02-10 00:11:44 +0000

    home changes

--- src/components/Sections/Title/Description.jsx
+++ src/components/Sections/Title/Description.jsx
@@ -1,29 +1,31 @@
 import React from "react"
 import { motion } from "framer-motion"
 
-const appear = {
+const rise = {
   hidden: {
-    opacity: 0,
+    y: "100%",
     transition: {
       ease: [0.51, 0.92, 0.24, 1],
       duration: 0.4,
     },
   },
   show: {
-    opacity: 0.5,
+    y: "0%",
     transition: {
       ease: [0.51, 0.92, 0.24, 1],
       duration: 0.4,
-      delay: 0.4,
+      delay: 0.2,
     },
   },
 }
 
 const Description = ({ description }) => {
   return (
-    <motion.span variants={appear} initial="hidden" animate="show">
-      {description}
-    </motion.span>
+    <div className="description">
+      <motion.span variants={rise} initial="hidden" animate="show">
+        {description}
+      </motion.span>
+    </div>
   )
 }
 
